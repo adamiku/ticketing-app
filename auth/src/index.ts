c0d2e2e37438a54c9504a77e7e7b33d1981132d8
Adam <adam_mikulas@mckinsey.com>
2024-02-09 21:02:21 +0100

    add errorhandling middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,9 @@
 import { json } from "body-parser";
 import express from "express";
+import "express-async-errors";
 
+import { NotFoundError } from "./errors/not-found-error";
+import { errorHandler } from "./middlewares/error-handler";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
@@ -18,6 +21,12 @@ app.use(signupRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(currentUserRouter);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!!!!");

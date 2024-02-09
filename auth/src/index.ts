e63099e644f1e1cba82829c41eef2c74bf565243
Adam <adam_mikulas@mckinsey.com>
2024-02-09 09:10:36 +0100

    add auth api endpoints

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,24 @@
 import { json } from "body-parser";
 import express from "express";
 
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+
 const app = express();
 app.use(json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there!");
+app.get("/api/users", (req, res) => {
+  console.log("yolo");
+  res.send("yolo");
 });
 
+app.use(signupRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(currentUserRouter);
+
 app.listen(3000, () => {
-  console.log("Listening on port 3000");
+  console.log("Listening on port 3000!!!!");
 });

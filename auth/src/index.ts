87e7baf9b7d86cc37611053f7ed9d6af70acec00
Adam <adam_mikulas@mckinsey.com>
2024-02-12 17:09:09 +0100

    add signup flow and jwt

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import "express-async-errors";
 import mongoose from "mongoose";
 
+import cookieSession from "cookie-session";
 import { NotFoundError } from "./errors/not-found-error";
 import { errorHandler } from "./middlewares/error-handler";
 import { currentUserRouter } from "./routes/current-user";
@@ -11,12 +12,15 @@ import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 
 const app = express();
-app.use(json());
+app.set("trust proxy", true);
 
-app.get("/api/users", (req, res) => {
-  console.log("yolo");
-  res.send("yolo");
-});
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(signupRouter);
 app.use(signinRouter);
@@ -30,6 +34,10 @@ app.all("*", async () => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
     console.log("Connected to mongodb");
